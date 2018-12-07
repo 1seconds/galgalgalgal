c9068b6b2009110d2d331ee53fb76d8637539cee
민수 정 <minsujeong@minsuui-MacBook-Pro.local>
2018-12-08 02:30:10 +0900

    기본 폴더 셋팅

---
+++ galgalgalgal/Assets/Scripts/Monster/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Move : MonoBehaviour {
+
+    [HideInInspector]
+    public Rigidbody2D rigidbody=null;
+	protected void Awake()
+	{
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.isKinematic = true;
+	}
+}
