7e8ae34682fcd1669761fbf4031882c0a2e392ba
Unknown <eltm360@gmail.com>
2018-12-08 20:18:15 +0900

    Auto stash before merge of "master" and "origin/master"
    Trap

--- galgalgalgal/Assets/Scripts/Trap/FireTrap.cs
+++ galgalgalgal/Assets/Scripts/Trap/FireTrap.cs
@@ -5,50 +5,51 @@ using UnityEngine;
 public class FireTrap : MonoBehaviour {
 
     public GameObject fire;
-	void Start ()
+    float destroyDist = -10f;
+    GameObject fireBall;
+    GameObject SplitBall;
+    GameObject leftFire;
+    GameObject rightFire;
+    
+    public float SplitTime = 0.5f;
+    void Start ()
     {
-        StartCoroutine(SplitFire());
+        if (fire.name == "FireBall2")
+            StartCoroutine(SplitFire());
     }
-
-    // Update is called once per frame
-    void Update ()
-    {
-		
-	}
     IEnumerator FireShot()
     {
-        float DestroyCycle = 4f;
+        float randomCycle;
         while (true)
         {
-            GameObject fireBall = Instantiate(fire, transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(DestroyCycle);
-            Destroy(fireBall);
+            randomCycle = Random.Range(0.3f, 0.6f);
+            fireBall = Instantiate(fire, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(randomCycle);
         }
     }
     IEnumerator SplitFire()
     {
         float ShotCycle = 1f;
-        float DestroyCycle = 10f;
-        float SplitTime = 0.5f;
+        float DestroyCycle = 0.5f;
         while (true)
         {
+            
             //직선으로 파이어볼 생성
-            GameObject fireBall = Instantiate(fire, transform.position, Quaternion.identity);
+            SplitBall = Instantiate(fire, transform.position, Quaternion.identity);
             yield return new WaitForSeconds(SplitTime);
 
-            //방향에 따른 파이어볼 생성
-            GameObject leftFire = Instantiate(fire);
+                //방향에 따른 파이어볼 생성
+            leftFire = Instantiate(fire);
 
             //발사각을 계산
-            leftFire.transform.position = fireBall.transform.position;
+            leftFire.transform.position = SplitBall.transform.position;
             leftFire.transform.eulerAngles = fire.transform.eulerAngles;
             leftFire.transform.eulerAngles += new Vector3(0, 0, -30);
-
-            //방향에 따른 파이어볼 생성
-            GameObject rightFire = Instantiate(fire);
+                //방향에 따른 파이어볼 생성
+            rightFire = Instantiate(fire);
 
             //발사각을 계산
-            rightFire.transform.position = fireBall.transform.position;
+            rightFire.transform.position = SplitBall.transform.position;
             rightFire.transform.eulerAngles = fire.transform.eulerAngles;
             rightFire.transform.eulerAngles += new Vector3(0, 0, 30);
 
@@ -60,5 +61,19 @@ public class FireTrap : MonoBehaviour {
             yield return new WaitForSeconds(ShotCycle);
         }
     }
-    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if (fire.name == "FireBall1")
+                StartCoroutine(FireShot());
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            StopAllCoroutines();
+        }
+    }
 }
