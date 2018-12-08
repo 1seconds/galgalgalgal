9a28748eefa92d0772a20e88c12e2a74da661c04
이성호 <kimjs7784@naver.com>
2018-12-08 18:44:30 +0900

    lee seongho PlayerMove

---
+++ galgalgalgal/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float MoveSpeed = 10f;
+    public float MamaximumSpeed = 2.0f;
+    public float JumpPower = 30f;
+    public float UpSpeed = 5f;
+
+    public bool isKeydownUpDown = false;
+    public bool isEnterSideTile = false;
+    public bool isEnterLadder = false;
+    public bool isJump = false;
+    
+
+    public void Update()
+    {
+
+        //좌우이동
+        if (isEnterSideTile == false)
+        {
+            float h = Input.GetAxis("Horizontal");
+            if (h != 0)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(MoveSpeed * h, 0, 0));
+            }
+            else if(h == 0)
+            {
+                Invoke("HorizontalStop", 0.0f);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+
+        //방향키 위아래
+        float Verti = Input.GetAxisRaw("Vertical");
+        if(Verti != 0)
+        {
+            isKeydownUpDown = true;
+        }
+        else
+        {
+            isKeydownUpDown = false;
+        }
+
+        //사다리 타기
+        if (isEnterLadder == true)
+        {
+            
+            if (Verti != 0)
+            {
+                transform.Translate(0, Verti * UpSpeed * Time.deltaTime, 0);
+            }
+        }
+
+
+        //최고속도 제한
+        if (gameObject.GetComponent<Rigidbody2D>().velocity.x > MamaximumSpeed)
+        {
+            Vector3 v = gameObject.GetComponent<Rigidbody2D>().velocity;
+            v.x = MamaximumSpeed;
+            gameObject.GetComponent<Rigidbody2D>().velocity = v;
+        }
+        else if (gameObject.GetComponent<Rigidbody2D>().velocity.x < -MamaximumSpeed)
+        {
+            Vector3 v = gameObject.GetComponent<Rigidbody2D>().velocity;
+            v.x = -MamaximumSpeed;
+            gameObject.GetComponent<Rigidbody2D>().velocity = v;
+        }
+
+
+
+        //점프
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gameObject.GetComponent<Rigidbody2D>().velocity.y ==0)
+            {
+                if (isEnterLadder == false)
+                {
+                    isJump = true;
+                    gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, JumpPower, 0));
+                }
+            }
+        }
+
+
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag=="Tile")
+        {
+            isJump = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag=="Side")
+        {
+            isEnterSideTile = true;
+        }
+
+        if(col.gameObject.tag=="ladder")
+        {
+            isEnterLadder = true;
+            if (isJump==true)//점프 중일 때
+            {
+                StartCoroutine(CheckJumpLadder());             
+            }
+            else
+            {
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            }
+        }
+        
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Side")
+        {
+            isEnterSideTile = false;
+        }
+
+        if (col.gameObject.tag == "Ladder")
+        {
+            isEnterLadder = false;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+    }
+    
+    public void HorizontalStop()
+    {
+        Vector3 v = gameObject.GetComponent<Rigidbody2D>().velocity;
+        v.x = 0;
+        gameObject.GetComponent<Rigidbody2D>().velocity = v;
+    }
+    public void VerticalStop()
+    {
+        Vector3 v = gameObject.GetComponent<Rigidbody2D>().velocity;
+        v.y = 0;
+        gameObject.GetComponent<Rigidbody2D>().velocity = v;
+    }
+
+    IEnumerator CheckJumpLadder()
+    {
+        do
+        {
+            if (isKeydownUpDown == true)
+            {
+                VerticalStop();
+                HorizontalStop();
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            }
+            yield return new WaitForSeconds(0.1f);
+        } while (isEnterLadder);
+    }
+
+
+    public void PlayerAnim_Idle()
+    {
+        gameObject.GetComponent<Animator>().SetInteger("state", 0);
+    }
+    public void PlayerAnim_Run()
+    {
+        gameObject.GetComponent<Animator>().SetInteger("state", 1);
+    }
+    public void PlayerAnim_Attack()
+    {
+        gameObject.GetComponent<Animator>().SetInteger("state", 2);
+    }
+    public void PlayerAnim_Die()
+    {
+        gameObject.GetComponent<Animator>().SetInteger("state", 3);
+    }
+}
